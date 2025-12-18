e53daf7f5fc4088cff967756f1b7464706cf4191
Anshul Bharat <anshul.ab17x@gmail.com>
2025-12-19 03:07:24 +0530

    db_scheema

---
+++ twoD-verse/packages/db/prisma/migrations/20251218191748_init_skeleton/migration.sql
@@ -0,0 +1,58 @@
+/*
+  Warnings:
+
+  - The values [Admin] on the enum `Role` will be removed. If these variants are still used in the database, this will fail.
+  - You are about to drop the `mapElements` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `createrId` to the `Space` table without a default value. This is not possible if the table is not empty.
+  - Made the column `avatarId` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "Role_new" AS ENUM ('creater', 'User');
+ALTER TABLE "User" ALTER COLUMN "role" TYPE "Role_new" USING ("role"::text::"Role_new");
+ALTER TYPE "Role" RENAME TO "Role_old";
+ALTER TYPE "Role_new" RENAME TO "Role";
+DROP TYPE "public"."Role_old";
+COMMIT;
+
+-- AlterTable
+ALTER TABLE "Space" ADD COLUMN     "createrId" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "avatarId" SET NOT NULL;
+
+-- DropTable
+DROP TABLE "mapElements";
+
+-- CreateTable
+CREATE TABLE "MapElements" (
+    "id" TEXT NOT NULL,
+    "mapId" TEXT NOT NULL,
+    "elementId" TEXT NOT NULL,
+    "x" INTEGER,
+    "y" INTEGER,
+
+    CONSTRAINT "MapElements_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MapElements_id_key" ON "MapElements"("id");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_avatarId_fkey" FOREIGN KEY ("avatarId") REFERENCES "Avatar"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Space" ADD CONSTRAINT "Space_createrId_fkey" FOREIGN KEY ("createrId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "spaceElements" ADD CONSTRAINT "spaceElements_spaceId_fkey" FOREIGN KEY ("spaceId") REFERENCES "Space"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "spaceElements" ADD CONSTRAINT "spaceElements_elementId_fkey" FOREIGN KEY ("elementId") REFERENCES "Element"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MapElements" ADD CONSTRAINT "MapElements_mapId_fkey" FOREIGN KEY ("mapId") REFERENCES "Map"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MapElements" ADD CONSTRAINT "MapElements_elementId_fkey" FOREIGN KEY ("elementId") REFERENCES "Element"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
