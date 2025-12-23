d86655e47f0c5080d3a7fb34f52b8e0a72c54f1e
Anshul Bharat <anshul.ab17x@gmail.com>
2025-12-23 12:30:37 +0530

    prisma_transaction

--- twoD-verse/apps/server/src/routes/v1/space.ts
+++ twoD-verse/apps/server/src/routes/v1/space.ts
@@ -1,26 +1,85 @@
 import express, { Router } from "express";
-
+import { userMiddleware } from "../../middleware/user.js";
+import { CreateSpaceSchema } from "../../types/index.js";
+import { prisma } from "@repo/db"; 
 export const spaceRouter: Router = express.Router();
 
 
-spaceRouter.post('/',(req,res)=> {
+spaceRouter.post('/', userMiddleware, async (req, res) => {
+  const parsedData = CreateSpaceSchema.safeParse(req.body);
 
-})
+  if (!parsedData.success) {
+    res.status(400).json({ message: "validation failed" });
+    return;
+  }
 
-spaceRouter.delete('/element',(req,res)=> {
+  const [widthStr, heightStr] = parsedData.data.dimensions.split("x");
 
-})
-spaceRouter.delete('/:spaceID',(req,res)=> {
+  const width = Number(widthStr);
+  const height = Number(heightStr);
 
-})
+
+  if (!parsedData.data.mapId) {
+    const space = await prisma.space.create({
+      data: {
+        name: parsedData.data.name,
+        width,
+        height,
+        createrId: req.userId!,
+      },
+    });
+
+    res.json({spaceId: space.id });
+    return;
+  }
+  const map = await prisma.map.findUnique({
+    where:{
+        id:parsedData.data.mapId
+    },select:{
+        mapElements:true
+    }
+  })
+  if(!map){
+    res.status(400).json({message:"Map doesn't exist!"})
+  }
+  await prisma.$transaction(async () => {
+       const space = await prisma.space.create({ 
+            data:{
+               name:parsedData.data.name,
+               width,
+               height,
+               createrId:req.userId!,
+            },
+        });
+      if(map?.mapElements && map.mapElements.length > 0) {
+        await prisma.spaceElements.createMany({
+            data:map?.mapElements.map(e => ({
+                spaceId: space.id,
+                elementId:e.elementId,
+                x:e.x!,
+                y:e.y!
+            }))
+        })
+        return space;
+        }
+        res.json({spaceId:space.id})
+   });
+});
+
+// spaceRouter.delete('/element',(req,res)=> {
+
+// })
+// spaceRouter.delete('/:spaceID',(req,res)=> {
+
+// })
  
-spaceRouter.get('/all',(req,res)=> {
+// spaceRouter.get('/all',(req,res)=> {
 
-})
+// })
 
-spaceRouter.post("/element", async (req, res) => {
+// spaceRouter.post("/element", async (req, res) => {
 
-})
+// })
 
 
-spaceRouter.get("/:spaceId",async (req, res) => {})
+// spaceRouter.get("/:spaceId",async (req, res) => {})
