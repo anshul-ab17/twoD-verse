7de3406d40d2d9b44dd20d37fcf3200f43b9cca4
Anshul Bharat <anshul.ab17x@gmail.com>
2025-12-25 01:57:24 +0530

    types

--- twoD-verse/packages/db/src/index.ts
+++ twoD-verse/packages/db/src/index.ts
@@ -1,11 +1,3 @@
 import { PrismaClient } from "./generated/prisma/index.js";
-import { PrismaPg } from "@prisma/adapter-pg";
-import pg from "pg";
-
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-export const prisma = new PrismaClient({
-  adapter: new PrismaPg(pool),
-});
+ 
+export const prisma=new PrismaClient();
