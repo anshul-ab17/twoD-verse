eae29cbcdc4e7c9ff21e5e1bc4d48fa7006f5e38
Anshul Bharat <anshul.ab17x@gmail.com>
2026-02-16 23:57:51 +0530

    home

--- twoD-verse/apps/verse/app/page.tsx
+++ twoD-verse/apps/verse/app/page.tsx
@@ -1,7 +1,25 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        
+    <div className="flex min-h-screen flex-col items-center justify-center gap-6  bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-4xl font-bold">TwoD verse</h1>
+
+      <div className="flex gap-4">
+        <Link
+          href="/signin"
+          className="rounded-md bg-black px-4 py-2 text-white"
+        >
+          Sign In
+        </Link>
+
+        <Link
+          href="/signup"
+          className="rounded-md border border-black px-4 py-2"
+        >
+          Sign Up
+        </Link>
+      </div>
     </div>
-  );
+  )
 }
