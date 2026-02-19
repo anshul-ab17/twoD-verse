f3dbadebf6b432892d978bb129d09166c511c659
Anshul Bharat <anshul.ab17x@gmail.com>
2026-02-19 13:27:19 +0530

    added db

---
+++ packages/db/index.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client/extension";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+export const client= new PrismaClient({
+  adapter,
+});
