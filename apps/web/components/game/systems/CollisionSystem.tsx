819432e9e9df3c1e159a3782a42a30bdde9a4027
Anshul Bharat <anshul.ab17x@gmail.com>
2026-02-25 14:00:00 +0530

    rewriting frontend

--- apps/web/components/game/systems/CollisionSystem.tsx
+++
@@ -1,9 +0,0 @@
-import * as Phaser from "phaser"
-
-export function setupCollisions(
-  scene: Phaser.Scene,
-  player: Phaser.Physics.Arcade.Sprite,
-  desks: Phaser.Physics.Arcade.StaticGroup
-) {
-  scene.physics.add.collider(player, desks)
-}
